e59c6c838a75ceb79faaf76bf7b2bb6998a4bf1c
bvincent1 <vincent@ualberta.ca>
2014-05-30 15:55:47 -0600

    organized imports

--- src/View/MapView.java
+++ src/View/MapView.java
@@ -1,14 +1,8 @@
 package view;
 
-import java.util.Random;
-
-import controller.*;
 import model.IslandRule;
 import model.Map;
 import model.MapGenerator;
-import model.RiverRule;
-import model.Rule;
-import model.Tile;
 
 public class MapView {
 	public static void main (String[] args){
