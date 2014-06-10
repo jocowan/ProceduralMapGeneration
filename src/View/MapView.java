ab27f765c365f179f9e3b2a250dddf8aa216578a
bvincent1 <vincent@ualberta.ca>
2014-06-10 16:00:47 -0600

    Added test draw

--- src/View/MapView.java
+++ src/View/MapView.java
@@ -1,16 +1,22 @@
 package view;
 
+import java.awt.Graphics;
+
 import model.IslandRule;
 import model.Map;
 import model.MapGenerator;
 import model.RiverRule;
 
 public class MapView {
-	public static void main (String[] args){
+	public Map getMap (String[] args){
 		Map map = MapGenerator.makeEmptyMap(10,10);
 		RiverRule rule = new RiverRule();
 		
-		map = rule.getRuling(map);		
-		map.printMap();
+		map = rule.getRuling(map);				
+		return map;
+	}
+	
+	public void printMap(Map theMap){
+		theMap.printMap();
 	}
 }
--- src/view/GameMainStart.java
+++ src/view/GameMainStart.java
@@ -3,8 +3,10 @@ package view;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 
-public class GameMain extends Applet implements Runnable {
+public class GameMainStart extends Applet implements Runnable {
 
 	@Override
 	public void init() {
@@ -41,7 +43,7 @@ public class GameMain extends Applet implements Runnable {
 	public void run() {
 		while(true){
 			// main game loop
-			
+			update();
 			try{
 				Thread.sleep(17);
 			}
@@ -49,5 +51,29 @@ public class GameMain extends Applet implements Runnable {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	@Override
+	public void update(Graphics g){
+		Image image = null;
+		Graphics second = null;
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+		
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+
+		g.drawImage(image, 0, 0, this);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawString("hello", 30, 30);
+
 	}
 }
