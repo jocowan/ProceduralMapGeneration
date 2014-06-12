334d364ded15739c671b22e13cd170e0a2edeada
bvincent1 <vincent@ualberta.ca>
2014-06-12 11:38:59 -0600

    added eclipse stuff

--- src/View/MapView.java
+++ src/View/MapView.java
@@ -1,6 +1,12 @@
 package view;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.net.URL;
+import java.applet.Applet;
 
 import model.IslandRule;
 import model.Map;
@@ -8,7 +14,8 @@ import model.MapGenerator;
 import model.RiverRule;
 
 public class MapView {
-	public Map getMap(String[] args) {
+	
+	public static Map getMap(String[] args) {
 		Map map = MapGenerator.makeEmptyMap(10, 10);
 		RiverRule rule = new RiverRule();
 
@@ -16,8 +23,15 @@ public class MapView {
 		return map;
 	}
 
-	public void printMap(Map theMap) {
+	public static void printMap(Map theMap, Graphics theGraphics, ImageObserver theObserver) {
+		
+		Image waterTile = Toolkit.getDefaultToolkit().getImage("../../resources/grass_tile.png");
+		Image grassTile = Toolkit.getDefaultToolkit().getImage("../../resources/grass_tile.png");
 		// theMap.printMap();
+		
+		theGraphics.drawString("help", 0, 0);
+		
+		System.out.println(grassTile.toString());
 
 		int lengthX = theMap.getLengthX();
 		int lengthY = theMap.getLengthY();
@@ -35,13 +49,20 @@ public class MapView {
 			for (j = 0; j < lengthY; j++) {
 				if (theMap.getTile(i, j).getSaturation() == 100) {
 					System.out.print(" w |");
+					
+					theGraphics.drawImage(waterTile, i+64, j+64, theObserver);
+					
 				} else if (theMap.getTile(i, j).getSaturation() == 0) {
 					System.out.print(" l |");
+					
+					theGraphics.drawImage(grassTile, i+64, j+64, theObserver);
 				} else if (theMap.getTile(i, j).getSaturation() == 0
 						&& theMap.getTile(i, j).getElevation() > 0) {
 					System.out.print(" e |");
 				} else if (theMap.getTile(i, j).getSaturation() == 50) {
 					System.out.print(" r |");
+					
+					theGraphics.drawImage(waterTile, i+64, j+64, theObserver);
 				} else {
 					System.out.print(" ? |");
 				}
@@ -55,4 +76,5 @@ public class MapView {
 		}
 		System.out.print("\n");
 	}
+
 }
--- src/view/GameMainStart.java
+++ src/view/GameMainStart.java
@@ -5,32 +5,40 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.ImageObserver;
 import java.net.URL;
 
+import model.Map;
+
 public class GameMainStart extends Applet implements Runnable {
-	
-	private Image image, waterTile, grassTile;
-	private Graphics second;
+
+	private Image backbuffer;
+	private Graphics backg;
 	private URL base;
+	private Map theNewMap;
+
+	int width, height;
 
 	@Override
 	public void init() {
-		setSize(800, 480);
-		setBackground(Color.cyan);
-		setFocusable(true);
+		width = getSize().width;
+		height = getSize().height;
+
+		setSize(width, height);
+
+		backbuffer = createImage(width, height);
 		
+
+		backg = backbuffer.getGraphics();
+
+
+		// setFocusable(true);
+
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Map View Alpha");
-		
-		try {
-			base = getDocumentBase();
-			System.out.print(base);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		waterTile = getImage(base, "../resources/water_tile.png");
-		grassTile = getImage(base, "../resources/grass_tile.png");
+
+		theNewMap = MapView.getMap(null);
+		MapView.printMap(theNewMap, backg, this);
 	}
 
 	@Override
@@ -50,46 +58,30 @@ public class GameMainStart extends Applet implements Runnable {
 	}
 
 	/**
-	 * main game run method
-	 * contains the main game loop to be threaded
+	 * main game run method contains the main game loop to be threaded
 	 */
 	@Override
 	public void run() {
-		while(true){
+		while (true) {
 			// main game loop
 			repaint();
-			try{
+			try {
 				Thread.sleep(17);
-			}
-			catch (InterruptedException e){
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	@Override
-	public void update(Graphics g){
-		image = null;
-		second = null;
-		if (image == null) {
-			image = createImage(this.getWidth(), this.getHeight());
-			second = image.getGraphics();
-		}
-		
-		second.setColor(getBackground());
-		second.fillRect(0, 0, getWidth(), getHeight());
-		second.setColor(getForeground());
-		paint(second);
+	public void update(Graphics g) {
 
-
-		g.drawImage(image, 0, 0, this);
+		g.drawImage(backbuffer, 0, 0, this);
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
-		g.drawString("hello", 30, 30);
-		g.drawImage(waterTile, 80, 80, this);
-		g.drawImage(grassTile, 144, 80, this);
-
+		MapView.printMap(theNewMap, g, this);
 	}
+
 }
